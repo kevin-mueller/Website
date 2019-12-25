4b4a9d6de2ee9cef19667201e4533e2e9eee60fb
Kevin Müller <kevin.mueller1@gmx.ch>
2019-12-25 17:57:03 +0100

    working on github api

--- Website/Source/API/GitHubWrapper.cs
+++ Website/Source/API/GitHubWrapper.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Octokit;
+using Microsoft.AspNetCore.Session;
 
 namespace Website.Source.API
 {
     public class GitHubWrapper
     {
+        private GitHubClient client;
+        public GitHubWrapper()
+        {
+            client = new GitHubClient(new ProductHeaderValue("Lucky2114"));
+        }
 
+        public async Task<string> GetUserRepositories()
+        {
+            var user = await client.User.Get("Lucky2114");
+            return user.PublicRepos.ToString();
+        }
     }
 }
